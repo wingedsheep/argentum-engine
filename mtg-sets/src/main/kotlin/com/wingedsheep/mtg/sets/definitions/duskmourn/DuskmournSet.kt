ccca11fa8eda38ff65f874b69837ecf53de2f907
Bjørn-Olav Strand <bolav@ikke.no>
2026-04-30 16:04:17 +0200

    Add Floodfarm Verge (DSK)

--- mtg-sets/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/duskmourn/DuskmournSet.kt
+++ mtg-sets/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/duskmourn/DuskmournSet.kt
@@ -15,6 +15,7 @@ object DuskmournSet {
 
     val allCards = listOf(
         EtherealArmor,
+        FloodfarmVerge,
         OptimisticScavenger,
         ShardmagesRescue,
         ShelteredByGhosts,
