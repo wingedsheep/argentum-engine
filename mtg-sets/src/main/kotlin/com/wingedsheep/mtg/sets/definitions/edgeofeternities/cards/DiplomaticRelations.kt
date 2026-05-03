e1d08b0f23a56eacd77fc277c592d7b604d5f3f9
Bjørn-Olav Strand <bolav@ikke.no>
2026-05-03 22:17:20 +0200

    Fix Diplomatic Relations

--- mtg-sets/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/edgeofeternities/cards/DiplomaticRelations.kt
+++ mtg-sets/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/edgeofeternities/cards/DiplomaticRelations.kt
@@ -1,13 +1,15 @@
 package com.wingedsheep.mtg.sets.definitions.edgeofeternities.cards
 
-import com.wingedsheep.sdk.dsl.card
-import com.wingedsheep.sdk.model.Rarity
 import com.wingedsheep.sdk.dsl.Effects
 import com.wingedsheep.sdk.dsl.Targets
-import com.wingedsheep.sdk.core.Keyword
+import com.wingedsheep.sdk.dsl.card
+import com.wingedsheep.sdk.model.Rarity
+import com.wingedsheep.sdk.scripting.effects.CompositeEffect
+import com.wingedsheep.sdk.scripting.targets.TargetCreature
+import com.wingedsheep.sdk.scripting.filters.unified.TargetFilter
 import com.wingedsheep.sdk.scripting.values.DynamicAmount
-import com.wingedsheep.sdk.scripting.values.EntityReference
 import com.wingedsheep.sdk.scripting.values.EntityNumericProperty
+import com.wingedsheep.sdk.scripting.values.EntityReference
 
 /**
  * Diplomatic Relations
@@ -20,20 +22,22 @@ val DiplomaticRelations = card("Diplomatic Relations") {
     typeLine = "Instant"
     oracleText = "Target creature you control gets +1/+0 and gains vigilance until end of turn. It deals damage equal to its power to target creature an opponent controls."
 
-    // Main spell effect
     spell {
-        val yourCreature = target("target creature you control", Targets.CreatureYouControl)
-        val opponentCreature = target("target creature an opponent controls", Targets.CreatureOpponentControls)
-        
-        // Get the power of your creature (after the +1/+0 bonus)
-        val creaturePower = DynamicAmount.Add(
-            DynamicAmount.EntityProperty(EntityReference.Target(0), EntityNumericProperty.Power),
-            DynamicAmount.Fixed(1)
+        val myCreature = target("creature you control", Targets.CreatureYouControl)
+        val theirCreature = target(
+            "up to one creature an opponent controls",
+            TargetCreature(optional = true, filter = TargetFilter.CreatureOpponentControls)
         )
-        
-        effect = Effects.ModifyStats(1, 0, yourCreature)
-            .then(Effects.GrantKeyword(Keyword.VIGILANCE, yourCreature))
-            .then(Effects.DealDamage(creaturePower, opponentCreature, damageSource = yourCreature))
+        effect = CompositeEffect(
+            listOf(
+                Effects.ModifyStats(1, 0, myCreature),
+                Effects.DealDamage(
+                    amount = DynamicAmount.EntityProperty(EntityReference.Target(0), EntityNumericProperty.Power),
+                    target = theirCreature,
+                    damageSource = myCreature
+                )
+            )
+        )
     }
 
     metadata {
